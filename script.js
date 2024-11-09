2464ef5480e83f8c11734c6408ec3fa5ec09e652
LuxTSP <clubitusf@gmail.com>
2024-11-09 01:36:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-function createRequest() {
-
-    if (document.getElementById("connecStatus").innerHTML == "disconnected ") {
-        document.getElementById('connecStatus').classList.add("statusY");
-        document.getElementById('connecStatus').classList.remove("statusN");
+//---------------------------------------------------------------------------------------------
+//                         Feedback Utilisateur + Variables
+//---------------------------------------------------------------------------------------------
+function connectionFeedback() {
+    if (statusC == true) {
+        document.getElementById("connecStatus").classList.add("statusY");
+        document.getElementById("connecStatus").classList.remove("statusN");
         document.getElementById("connecStatus").innerHTML = "connected ";
-    } else if (document.getElementById("connecStatus").innerHTML == null) {
-        document.getElementById('connecStatus').classList.add("statusN");
-        document.getElementById("connecStatus").innerHTML = "disconnected ";
     } else {
-        document.getElementById('connecStatus').classList.add("statusN");
-        document.getElementById('connecStatus').classList.remove("statusY");
+        document.getElementById("connecStatus").classList.add("statusN");
+        document.getElementById("connecStatus").classList.remove("statusY");
         document.getElementById("connecStatus").innerHTML = "disconnected ";
     }
+}
+function printToUser(){
+    const p = document.createElement("p");
+    document.getElementById("chat").appendChild(p);
+    
 }
 function cacheValues() {
     roomCode = document.getElementById("code").innerHTML;
     fileSent = document.getElementById("upFile").innerHTML;
     messageSent = document.getElementById("message").innerHTML;
+    connectionFeedback();  
 }
+
+let statusC = false;
+let messageRecieved;
+let fileRecieved;
+cacheValues();
