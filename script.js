589d3079c74da2440e474d09a8a889a8507cbcdd
LuxTSP <clubitusf@gmail.com>
2024-11-12 14:51:29 +0100

    Edited some functions
    - File feedback need improvement
    - IsConnected() function need to be made
    - Server will arrive after base code is done

--- script.js
+++ script.js
@@ -12,39 +12,55 @@ function connectionFeedback() {
         document.getElementById("connecStatus").innerHTML = "disconnected ";
     }
 }
-function printToUser(messageToShow){
+function printToUser(messageToShow) {
     var message = messageToShow;
     const p = document.createElement("p");
     document.getElementById("chatbox").appendChild(p).innerHTML = message;
-    
+}
+function downToUser(file,message) {
+    const p = document.createElement("a");
+    document.getElementById("chatbox").appendChild(p).href = file;
+    document.getElementById("chatbox").appendChild(p).innerHTML = message;
+    document.getElementById("chatbox").appendChild(p).download = 'file';
 }
 function cacheValues() {
-    roomCode = document.getElementById("code").innerHTML;
+    roomCode = document.getElementById("code").value;
     fileSent = document.getElementById("upFile").innerHTML;
-    messageSent = document.getElementById("message").innerHTML;
-    connectionFeedback();  
+    messageSent = document.getElementById("message").value;
+    connectionFeedback();
+}
+function isConnected() {
+    return bool;
 }
 
 let statusC = false;
 let messageRecieved;
 let fileRecieved;
-let user1 = 'you : ';
-let user2 = 'anon : ';
+let user1 = "you : ";
+let user2 = "anon : ";
 cacheValues();
 
 //----------------------------------------------------------------------------------------------
 //                                  User Buttons
 //----------------------------------------------------------------------------------------------
-function createRequest(){
-    if (statusC == false){
+function createRequest() {
+    if (statusC == false) {
         statusC = true;
-    }
-    else{
+    } else {
         statusC = false;
     }
-    message = 'bobby is a fucking bitch he deserves to die, fuck angel asked me for new content i want to fucking die help me, get me out of here';
-    userT = user1;
-    messageToShow = userT + message;
-    printToUser(messageToShow);
     cacheValues();
+}
+function sendMessage() {
+    cacheValues();
+    message = user1 + messageSent;
+    document.getElementById("message").value = "";
+    printToUser(message);
+}
+function sendFile() {
+    cacheValues();
+    message = fileSent;
+    var prntmss = user1 + "Download";
+    document.getElementById("upFile").innerHTML = null;
+    downToUser(message, prntmss);
 }
